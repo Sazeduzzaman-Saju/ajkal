8355a76a3c005465e2631839d18bcddcbb6b9c38
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-02-15 21:18:28 +0600

    Header & Footer Done

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,148 +1,196 @@
 import React from "react";
 import "./Footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <div className="container">
-      <footer className="py-5">
-        <div className="row">
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-muted">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-md-5 offset-md-1 mb-3">
-            <form>
-              <h5>Subscribe to our newsletter</h5>
-              <p>Monthly digest of what's new and exciting from us.</p>
-              <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                <label htmlFor="newsletter1" className="visually-hidden">
-                  Email address
-                </label>
-                <input
-                  id="newsletter1"
-                  type="text"
-                  className="form-control"
-                  placeholder="Email address"
-                />
-                <button className="btn btn-primary" type="button">
-                  Subscribe
-                </button>
+    <div className="container-fluid bg-light">
+      <div className="container px-0">
+        <footer className="py-5">
+          <div className="row">
+            <div className="col-lg-3">
+              <h5>মোবাইল অ্যাপস ডাউনলোড করুন</h5>
+              <p className="footer-devider"></p>
+              <div className="d-flex align-items-center pt-4 pb-2">
+                <div className="pe-1">
+                  <img
+                    className="img-fluid"
+                    width={140}
+                    src="../../../public/playstore.webp"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    className="img-fluid"
+                    width={140}
+                    src="../../../public/applestore.webp"
+                    alt=""
+                  />
+                </div>
               </div>
-            </form>
+              <h5 className="pt-4">অনুসরণ করুন</h5>
+              <div className="footer-social pt-2">
+                <a href="#" className="ms-0">
+                  <FaFacebookF />
+                </a>
+                <a href="#" className="">
+                  <FaTwitter />
+                </a>
+                <a href="#" className="">
+                  <FaYoutube />
+                </a>
+                <a href="#" className="">
+                  <AiFillInstagram />
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <h5>আজকের পত্রিকা</h5>
+              <p className="footer-devider"></p>
+              <div className="row">
+                <div className="col-lg-12">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        প্রথম পাতা
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        খেলা
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        শুভসংঘ
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        শেষের পাতা
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        খবর
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        দেশে দেশে
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <h5>অনলাইন</h5>
+              <p className="footer-devider"></p>
+              <div className="row">
+                <div className="col-lg-12">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        জাতীয়
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        সারাবাংলা
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        বিশ্ব
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        বাণিজ্য
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        বিনোদন
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        খেলাধুলা
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3">
+              <h5>বিজ্ঞাপন</h5>
+              <p className="footer-devider"></p>
+              <div className="row">
+                <div className="col-lg-12">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <Link className="nav-link footer-links" to="/">
+                        মূল্য তালিকা (প্রিন্ট ভার্সন)
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-          <p>© ২০২৪ দৈনিক আজকাল, Inc. All rights reserved.</p>
-          <ul className="list-unstyled d-flex">
-            <li className="ms-3">
-              <a className="link-dark" href="#">
-                <svg className="bi" width={24} height={24}>
-                  <use xlinkHref="#twitter" />
-                </svg>
-              </a>
-            </li>
-            <li className="ms-3">
-              <a className="link-dark" href="#">
-                <svg className="bi" width={24} height={24}>
-                  <use xlinkHref="#instagram" />
-                </svg>
-              </a>
-            </li>
-            <li className="ms-3">
-              <a className="link-dark" href="#">
-                <svg className="bi" width={24} height={24}>
-                  <use xlinkHref="#facebook" />
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </footer>
+          <div className="row pt-5">
+            <div className="col-lg-12">
+              <div className="d-flex align-items-center justify-content-between ">
+                <p>প্রধান সম্পাদক : ইমদাদুল হক মিলন</p>
+                <div>
+                  <ul
+                    className="navbar-nav flex-row"
+                    style={{ display: "flex !important" }}
+                  >
+                    <li className="nav-item pe-5">
+                      <Link className="nav-link footer-links" to="/">
+                        আমাদের
+                      </Link>
+                    </li>
+                    <li className="nav-item pe-5">
+                      <Link className="nav-link footer-links" to="/">
+                        সম্পর্কে
+                      </Link>
+                    </li>
+                    <li className="nav-item pe-5">
+                      <Link className="nav-link footer-links" to="/">
+                        শর্তাবলী
+                      </Link>
+                    </li>
+                    <li className="nav-item pe-5">
+                      <Link className="nav-link footer-links" to="/">
+                        গোপনীয়তা নীতি
+                      </Link>
+                    </li>
+                    <li className="nav-item pe-5">
+                      <Link className="nav-link footer-links" to="/">
+                        যোগাযোগ করুন
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-lg-12 pt-5">
+              <p className="text-center text-muted">স্বত্ব © ২০২৪ আজকাল</p>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -6,12 +6,12 @@ import { FaTwitter } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 
-const Header = () => {
+function NavScrollExample() {
   return (
     <>
       <div className="container">
         {/* Advertisement Banner */}
-        <div className="row">
+        <div className="row top-bar-area">
           <div className="col-lg-12 px-0">
             <img
               className="img-fluid top-add"
@@ -21,7 +21,7 @@ const Header = () => {
           </div>
         </div>
         {/* Top Bar Area */}
-        <div className="row top-bar-area align-items-center">
+        <div className="row top-bar-area align-items-center top-bar-area">
           <div className="col-lg-6">
             <p className="m-0 p-0">
               ঢাকা, বৃহস্পতিবার ১৫ ফেব্রুয়ারি ২০২৪, ২ ফাল্গুন ১৪৩০, ০৪ শাবান
@@ -66,170 +66,139 @@ const Header = () => {
             </div>
           </div>
         </div>
-        {/* Main Navigation Menu */}
-        <div className="row">
-          <div className="col-lg-12 px-0 sticky-top">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light top-bar-area">
-              <div className="container-fluid">
-                <div
-                  className="collapse navbar-collapse"
-                  id="navbarTogglerDemo01"
-                >
-                  <a className="navbar-brand" href="#">
-                    <img src="https://i.ibb.co/6D35WjX/logo.png" alt="" />
+      </div>
+      <nav className="navbar navbar-expand-lg bg-white sticky-top flex-column main-menu pt-0">
+        <div className="container px-0 pb-1 pt-1">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div>
+            <a className="navbar-brand" href="#">
+              <img src="https://i.ibb.co/6D35WjX/logo.png" alt="" />
+            </a>
+          </div>
+          <div className="search-container">
+            <form action="/search" method="get">
+              <input
+                className="search expandright"
+                id="searchright"
+                type="search"
+                name="q"
+                placeholder="Search"
+              />
+              <label className="buttonss searchbutton" for="searchright">
+                <span className="mglass">&#9906;</span>
+              </label>
+            </form>
+          </div>
+        </div>
+        <div className="container px-0 main-menu">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks active" aria-current="page" to="#">
+                  যুক্তরাষ্ট্র
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  বাংলাদেশ
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  নিউইয়র্ক
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  সারাবিশ্ব
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  প্রবাস
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  বিনোদন
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  খেলার মাট
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  স্বাস্থ্য
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  ধর্ম
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  সংকলন
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3">
+                <Link className="nav-link navlinks" aria-current="page" to="#">
+                  আর্কাইভ
+                </Link>
+              </li>
+              <li className="nav-item menu-border pe-3 ps-3 ">
+                <div className="dropdown main-menu">
+                  <a
+                    className="border-0 bg-transparent pt-2 text-white"
+                    type="button"
+                    id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    style={{ fontSize: "18px" }}
+                  >
+                    আরও +
                   </a>
-                  <div className="mx-auto mb-2 mb-lg-0">
-                    <marquee
-                      behavior="scroll"
-                      direction="left"
-                      style={{ width: "61.875rem", color: "black" }}
-                    >
-                      প্রতিবছর বিশ্বে অন্তত চার লাখ শিশু ক্যান্সারে আক্রান্ত হয়
-                      । উন্নত দেশগুলোতে ক্যান্সার থেকে রোগীর সু । ডেইলি স্টারের
-                      নির্বাহী সম্পাদক আশফাক ও স্ত্রীকে জিজ্ঞাসাবাদ চলছে ।
-                      শ্রীলঙ্কা সিরিজের দল নির্বাচন নিয়ে প্রশ্ন তুললেন
-                      সালাউদ্দিন । ১০ মাস পর বিয়ের খবর জানালেন ‘পালকী’ খ্যাত
-                      নায়িকা
-                    </marquee>
-                  </div>
-                  <div class="search-container">
-                    <form action="/search" method="get">
-                      <input
-                        class="search expandright"
-                        id="searchright"
-                        type="search"
-                        name="q"
-                        placeholder="Search"
-                      />
-                      <label class="buttonss searchbutton" for="searchright">
-                        <span class="mglass">&#9906;</span>
-                      </label>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </nav>
-            <nav className="navbar navbar-expand-lg navbar-light main-menu py-0">
-              <div className="container-fluid">
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarTogglerDemo01"
-                  aria-controls="navbarTogglerDemo01"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon" />
-                </button>
-                <div
-                  className="collapse navbar-collapse"
-                  id="navbarTogglerDemo01"
-                >
-                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link
-                        className="nav-link active"
-                        aria-current="page"
-                        to="#"
-                      >
-                        যুক্তরাষ্ট্র
-                      </Link>
+                  <ul
+                    className="dropdown-menu"
+                    style={{ borderBottom: "none" }}
+                    aria-labelledby="dropdownMenuButton1"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
                     </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        বাংলাদেশ
-                      </Link>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
                     </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        নিউইয়র্ক
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        সারাবিশ্ব
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        প্রবাস
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        বিনোদন
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        খেলার মাট
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        স্বাস্থ্য
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        ধর্ম
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        সংকলন
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3">
-                      <Link className="nav-link" aria-current="page" to="#">
-                        আর্কাইভ
-                      </Link>
-                    </li>
-                    <li className="nav-item menu-border pe-3 ps-3 ">
-                      <div class="dropdown main-menu">
-                        <a
-                          class="border-0 bg-transparent pt-2 text-white"
-                          type="button"
-                          id="dropdownMenuButton1"
-                          data-bs-toggle="dropdown"
-                          aria-expanded="false"
-                          style={{ fontSize: "18px" }}
-                        >
-                          আরও +
-                        </a>
-                        <ul
-                          class="dropdown-menu"
-                          style={{ borderBottom: "none" }}
-                          aria-labelledby="dropdownMenuButton1"
-                        >
-                          <li>
-                            <a class="dropdown-item" href="#">
-                              Action
-                            </a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">
-                              Another action
-                            </a>
-                          </li>
-                          <li>
-                            <a class="dropdown-item" href="#">
-                              Something else here
-                            </a>
-                          </li>
-                        </ul>
-                      </div>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
                     </li>
                   </ul>
                 </div>
-              </div>
-            </nav>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
     </>
   );
-};
+}
 
-export default Header;
+export default NavScrollExample;
