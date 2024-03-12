2d163e48403ee6e27d58a3a90211271ff4680469
Sazeduzzaman-Saju <szamansaju@gmail.com>
2024-03-13 03:37:39 +0600

    all api added succesfully done

--- src/Pages/Categories/CategoryFeature/CategoryFeature.jsx
+++ src/Pages/Categories/CategoryFeature/CategoryFeature.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CategoryNewsSlider from "./CategoryNewsSlider";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const CategoryFeature = ({ singleNews }) => {
   console.log(singleNews);
@@ -12,6 +13,7 @@ const CategoryFeature = ({ singleNews }) => {
         </div>
         {singleNews.map((data) => (
           <div className="col-sm-4" key={data.id}>
+            <Link to={`/news/${data.id}`}>
             <div className="card rounded-0 border-0 shadow-sm ">
               <div className="card-header p-0"></div>
               <div className="card-body p-0">
@@ -23,22 +25,27 @@ const CategoryFeature = ({ singleNews }) => {
                   />
                 </div>
                 <div className="p-2">
-                  <h5 className="pt-2 main_color">{data.news_title.slice(0,24)}</h5>
-                  <p className="">
-                    {data.news_short_brief.slice(0,59)}...
+                  <h5 className="pt-2 main_color">
+                    {data.news_title.slice(0, 24)}
+                  </h5>
+                  <p className="">{data.news_short_brief.slice(0, 59)}...</p>
+                  <p className="text-muted mb-1" style={{ fontSize: "12px" }}>
+                    {data.news_time.slice(10)}
                   </p>
-                  <p className="text-muted mb-1" style={{fontSize: '12px'}}>{data.news_time.slice(10)}</p>
-                  <p className="pb-0 mb-0 main_color sec_color">{data.category_name_bangla}</p>
+                  <p className="pb-0 mb-0 main_color sec_color">
+                    {data.category_name_bangla}
+                  </p>
                 </div>
               </div>
             </div>
+            </Link>
           </div>
         ))}
       </div>
       <div className="row">
         <div className="d-flex justify-content-center py-3 pt-5">
           <img
-          className="img-fluid"
+            className="img-fluid"
             src="https://www.adgully.com/banners/ezgif_com_gif_maker__1__183907.gif"
             alt=""
           />
